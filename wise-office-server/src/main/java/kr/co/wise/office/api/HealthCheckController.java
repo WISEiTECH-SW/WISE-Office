8d88787f249e734469b34b0ab493e7a3053499d7
jhryu <jhryu@gmail.com>
2025-08-13 08:39:18 +0900

    feat : add controller for swagger docs

--- wise-office-server/src/main/java/kr/co/wise/office/api/HealthCheckController.java
+++ wise-office-server/src/main/java/kr/co/wise/office/api/HealthCheckController.java
@@ -1,10 +1,12 @@
 package kr.co.wise.office.api;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
+@Tag(name = "health check", description = "spring 서버 정상 작동 확인")
 public class HealthCheckController {
     
 
