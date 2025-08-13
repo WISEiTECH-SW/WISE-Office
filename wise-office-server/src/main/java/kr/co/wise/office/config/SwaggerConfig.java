6c001a508bc186216d6a44256040e3bfdff9f069
jhryu <jhryu@gmail.com>
2025-08-13 16:38:08 +0900

    feat : add swagger jwt config

--- wise-office-server/src/main/java/kr/co/wise/office/config/SwaggerConfig.java
+++ wise-office-server/src/main/java/kr/co/wise/office/config/SwaggerConfig.java
@@ -1,13 +1,15 @@
 package kr.co.wise.office.config;
 
-import java.util.List;
-
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig{
@@ -19,6 +21,11 @@ public class SwaggerConfig{
                     .title("백오피스 API")
                     .description("백오피스 개발 API 명세서입니다.")
                     .version("1.0.0"))
+            .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+            .components(new Components().addSecuritySchemes("bearerAuth", new SecurityScheme()
+                    .name("bearerAuth").type(SecurityScheme.Type.HTTP)
+                    .scheme("bearer")
+                    .bearerFormat("JWT")))
             .servers(List.of(
                 new Server().url("http://localhost:8080").description("테스트 개발 서버")
             ));
