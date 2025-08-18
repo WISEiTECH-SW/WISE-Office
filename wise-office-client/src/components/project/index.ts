62832c6e93ddae546b1efe45e838341873bb75a8
jwchoi <jwchoi@wise.co.kr>
2025-08-18 10:29:44 +0900

    feat : Change tsx file Naming

--- wise-office-client/src/components/project/index.ts
+++ wise-office-client/src/components/project/index.ts
@@ -1,6 +1,6 @@
 export { addLog } from "./addLog";
 
-export { default as ProjectParticipants } from "./projectParticipants";
-export { default as ProjectParticipantItem } from "./projectParticipantItem";
+export { default as ProjectParticipants } from "./ProjectParticipants";
+export { default as ProjectParticipantItem } from "./ProjectParticipantItem";
 
-export * from "./types";
+// export * from "./types";
