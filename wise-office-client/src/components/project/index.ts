2200d2a759e424d850cd6bf6b3c87b94949452bd
jwchoi <jwchoi@wise.co.kr>
2025-08-18 10:54:54 +0900

    feat : Seperate project-page Project Information

--- wise-office-client/src/components/project/index.ts
+++ wise-office-client/src/components/project/index.ts
@@ -11,4 +11,6 @@ export { default as ProjectLog } from "./ProjectLog";
 // Right-Sidebar
 export { default as ProjectAttendantList } from "./ProjectAttendantList";
 
-// export * from "./types";
+export { default as ProjectDetails } from "./ProjectDetails";
+export { default as ProjectInfoItem } from "./ProjectInfoItem";
+export * from "./types";
