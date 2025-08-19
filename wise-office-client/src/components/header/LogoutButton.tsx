7e7b5360d618a1abb8a97b1f600d810ea36ffb25
shoh0011 <shoh@wise.co.kr>
2025-08-19 15:13:21 +0900

    feat : Implement logout by clearing cookie

--- wise-office-client/src/components/header/LogoutButton.tsx
+++ wise-office-client/src/components/header/LogoutButton.tsx
@@ -1,7 +1,16 @@
 export default function LogoutButton() {
+    const deleteCookie = () => {
+        document.cookie = "jwt=; Max-age=0; Path=/";
+
+        location.reload();
+    };
+
     return (
-        <a className="inline-flex items-center justify-center gap-3 rounded-lg border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-blue-700 shadow-sm hover:bg-gray-100">
+        <button
+            onClick={deleteCookie}
+            className="inline-flex items-center justify-center gap-3 rounded-lg border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-blue-700 shadow-sm hover:bg-gray-100"
+        >
             <span className="text-blue-700">로그아웃</span>
-        </a>
+        </button>
     );
 }
--- wise-office-client/src/pages/api/auth/me.ts
+++ wise-office-client/src/pages/api/auth/me.ts
@@ -5,9 +5,9 @@ export default function handle(req: NextApiRequest, res: NextApiResponse) {
 
     if (token) {
         res.status(200).json({ loggedIn: true });
-        // console.log("로그인되었습니다.");
+        console.log("로그인 상태입니다.");
     } else {
         res.status(200).json({ loggedIn: false });
-        // console.log("로그인해주세요.");
+        console.log("로그아웃 상태입니다.");
     }
 }
