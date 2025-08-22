9e780be70fb7f49c2b46ad4b7be77082eeac69de
jysur <m15701313@gmail.com>
2025-08-22 18:05:38 +0900

    Revert "fix : delete console"
    This reverts commit 58315bbeac4ee841b6a144d6c03eb5925ae8da2b.

--- wise-office-client/src/pages/account.tsx
+++ wise-office-client/src/pages/account.tsx
@@ -30,6 +30,7 @@ export default function Account() {
             rank,
         };
         try{
+            console.log("수정될 값: ", req);
             const newProfileInfo = await updateProfileInfo(req);
             console.log(newProfileInfo);
         } catch(error){
