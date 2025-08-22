6539411215a032beb2a9f0a410f9dabdb3cc5722
shoh0011 <shoh@wise.co.kr>
2025-08-22 11:15:18 +0900

    refactor : rename server url variable

--- wise-office-client/src/components/header/LoginButton.tsx
+++ wise-office-client/src/components/header/LoginButton.tsx
@@ -1,5 +1,5 @@
 export default function GoogleLoginButton() {
-    const server = process.env.SERVER_URL!;
+    const server = process.env.NEXT_PUBLIC_SERVER_URL!;
     const loginUrl = `${server}/oauth2/authorization/google`;
 
     return (
