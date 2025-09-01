2c16914166845a344a5b26de3991c6d3603f99c4
jwchoi <jwchoi@wise.co.kr>
2025-09-01 10:17:10 +0900

    feat : Add project detail item

--- wise-office-client/src/components/project/ProjectInfoContainer.tsx
+++ wise-office-client/src/components/project/ProjectInfoContainer.tsx
@@ -1,7 +1,7 @@
 import ProjectInfoItem from "./ProjectInfoItem";
 import type { ProjectInfo } from "@/types/project";
 import { calculationDuration } from "@/lib/common/util";
-import { Calendar, TrendingUp, User, Users } from "lucide-react";
+import { Calendar, TrendingUp, User, Users, NotepadText } from "lucide-react";
 
 type ProjectContainerProps = {
     projectInfo: ProjectInfo;
@@ -16,27 +16,29 @@ export default function ProjectInfoContainer({
 }: ProjectContainerProps) {
     const duration = calculationDuration(projectInfo.start);
     return (
-        <div className="bg-white rounded-lg shadow-sm p-6 mb-6">
+        <div className="bg-white rounded-lg shadow-sm p-6 my-6">
             <div className="flex justify-between items-center mb-4">
                 <h1 className="text-2xl font-bold text-gray-800">
                     {projectInfo.projectTitle}
                 </h1>
-                <div className="flex gap-2">
-                    <button
-                        onClick={onEdit}
-                        className="px-4 py-2 bg-blue-500 text-white text-sm rounded hover:bg-blue-600 cursor-pointer"
-                    >
-                        수정
-                    </button>
-                    <button
-                        onClick={onDelete}
-                        className="px-4 py-2 bg-gray-500 text-white text-sm rounded hover:bg-gray-600 cursor-pointer"
-                    >
-                        삭제
-                    </button>
-                </div>
+                {projectInfo.canModify && (
+                    <div className="flex gap-2">
+                        <button
+                            onClick={onEdit}
+                            className="px-4 py-2 bg-blue-500 text-white text-sm rounded hover:bg-blue-600 cursor-pointer"
+                        >
+                            수정
+                        </button>
+                        <button
+                            onClick={onDelete}
+                            className="px-4 py-2 bg-gray-500 text-white text-sm rounded hover:bg-gray-600 cursor-pointer"
+                        >
+                            삭제
+                        </button>
+                    </div>
+                )}
             </div>
-            <div className="grid grid-cols-4 gap-6">
+            <div className="grid grid-cols-4 gap-6 mb-6">
                 <ProjectInfoItem
                     icon={<Calendar className="w-7 h-7 text-blue-600" />}
                     label="프로젝트 기간"
@@ -58,6 +60,11 @@ export default function ProjectInfoContainer({
                     value={`${projectInfo.attendant.length + 1}명`}
                 />
             </div>
+            <ProjectInfoItem
+                icon={<NotepadText className="w-7 h-7 text-blue-600" />}
+                label="프로젝트 설명"
+                value={projectInfo.detail}
+            />
         </div>
     );
 }
