b13c495a225908324382999248f35641be65586e
shoh0011 <shoh@wise.co.kr>
2025-09-09 16:18:02 +0900

    feat : Fix account page default profile

--- wise-office-client/src/components/account/UserProfile.tsx
+++ wise-office-client/src/components/account/UserProfile.tsx
@@ -1,20 +1,20 @@
 import { Profile } from "@/types/profile";
 import Image from "next/image";
-interface UserProfileProps{
-    props:Profile;
+interface UserProfileProps {
+    props: Profile;
 }
 
-export default function UserProfile({props}:UserProfileProps) {
+export default function UserProfile({ props }: UserProfileProps) {
     return (
-    <div className="flex flex-col items-center">
-        <Image
-        className="w-24 h-24 rounded-full bg-gray-300 object-cover"
-        src={props.imageUrl ?? "https://img.freepik.com/free-vector/blue-circle-with-white-user_78370-4707.jpg?semt=ais_hybrid&w=740&q=80"}
-        width={48}
-        height={48}
-        alt=""
-        />
-        <p className="mt-3 font-semibold">{props.name}</p>
-    </div>
+        <div className="flex flex-col items-center">
+            <Image
+                className="w-24 h-24 rounded-full bg-gray-300 object-cover"
+                src={props.imageUrl ?? "assets/default_profile.jpg"}
+                width={48}
+                height={48}
+                alt=""
+            />
+            <p className="mt-3 font-semibold">{props.name}</p>
+        </div>
     );
 }
