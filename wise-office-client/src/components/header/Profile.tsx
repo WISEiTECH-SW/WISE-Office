2b4f95b48b36d1ae0aef6561ef747649ca793a6d
shoh0011 <shoh@wise.co.kr>
2025-09-09 15:45:54 +0900

    feat : Change header default profile image

--- wise-office-client/src/components/header/Profile.tsx
+++ wise-office-client/src/components/header/Profile.tsx
@@ -16,8 +16,8 @@ export default function Profile() {
             className="rounded-full border border-white bg-white shadow-sm hover:bg-gray-100 cursor-pointer"
         >
             <Image
-                src={profile?.imageUrl ?? "/assets/Google.png"}
-                alt={profile?.name ?? "👤"}
+                src={profile?.imageUrl ?? "/assets/default_profile.jpg"}
+                alt=""
                 width={36}
                 height={36}
                 className="w-9 h-9 rounded-full object-cover"
