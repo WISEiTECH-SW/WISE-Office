5b0dae3b5e0ff390f8f70b15fb0ecb74ea1ca91c
shoh0011 <shoh@wise.co.kr>
2025-09-10 10:37:06 +0900

    feat : Add profile image update

--- wise-office-client/src/components/account/UserProfile.tsx
+++ wise-office-client/src/components/account/UserProfile.tsx
@@ -1,19 +1,62 @@
 import { Profile } from "@/types/profile";
-import Image from "next/image";
+import { useRef, useState } from "react";
+import { updateProfileImage } from "@/services/members";
+import { toastMessage } from "@/lib/common/toastMessage";
+import { useProfileStore } from "@/store/useProfileStore";
+import ProfileImage from "../header/ProfileImage";
+
 interface UserProfileProps {
     props: Profile;
 }
 
 export default function UserProfile({ props }: UserProfileProps) {
+    const [, setProfileImage] = useState(props.imageUrl);
+    const { profile, setProfile } = useProfileStore();
+    const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+    const handleButtonClick = () => {
+        fileInputRef.current?.click();
+    };
+
+    const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+        if (file) {
+            const res = await updateProfileImage(file);
+            setProfileImage(res);
+            setProfile({
+                ...(profile ?? props),
+                imageUrl: res,
+            });
+            toastMessage.success("프로필 이미지가 변경되었습니다.");
+        }
+    };
+
     return (
-        <div className="flex flex-col items-center">
-            <Image
-                className="w-24 h-24 rounded-full bg-gray-300 object-cover"
-                src={props.imageUrl ?? "assets/default_profile.jpg"}
-                width={48}
-                height={48}
-                alt=""
-            />
+        <div className="flex flex-col items-center gap-5">
+            <div className="relative">
+                <div className="w-36 h-36 rounded-full border-2 border-white">
+                    <ProfileImage type="account" />
+                </div>
+                <button
+                    type="button"
+                    onClick={handleButtonClick}
+                    className="absolute bottom-0 right-0 flex items-center justify-center w-10 h-10 rounded-full bg-gray-50 hover:bg-gray-300 cursor-pointer shadow"
+                >
+                    <img
+                        src="assets/camera.png"
+                        alt="Camera"
+                        className="w-5 h-5"
+                    />
+                </button>
+                <input
+                    type="file"
+                    accept="image/*"
+                    ref={fileInputRef}
+                    onChange={handleFileChange}
+                    className="hidden"
+                />
+            </div>
+
             <p className="mt-3 font-semibold">{props.name}</p>
         </div>
     );
