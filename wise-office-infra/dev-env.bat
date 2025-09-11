473039c868d993550e3dbe0d872fc090020bf40b
shoh0011 <shoh@wise.co.kr>
2025-09-11 14:36:07 +0900

    feat : Connect logout api

--- wise-office-infra/dev-env.bat
+++ wise-office-infra/dev-env.bat
@@ -3,7 +3,7 @@ REM 해당 파일은 서버 전체 환경을 구동할 때 사용합니다.
 REM jhryu 2025-08-27
 
 
-docker-compose -f docker-compose.yml -p wise-server --env-file=./.env.dev up -d --wait
+docker-compose -f docker-compose.yml -p wise-server --env-file=./.env.dev up --build -d --wait
 
 echo "Backend server initialization complete"
 echo "You can access the server at: http://localhost:8080/"
