747a119671382b7e1088b9a7bc7d68bc27488e31
shoh0011 <shoh@wise.co.kr>
2025-09-16 10:34:26 +0900

    feat : Apply mobile css to user page

--- wise-office-client/src/components/account/ProjectCardsMy.tsx
+++ wise-office-client/src/components/account/ProjectCardsMy.tsx
@@ -22,7 +22,7 @@ export default function ProjectCardsMy({ props }: UserProfileProps) {
                         </li>
                     ))
                 ) : (
-                    <li className="text-gray-500">
+                    <li className="text-gray-400 py-8">
                         등록된 프로젝트가 없습니다.
                     </li>
                 )}
@@ -46,15 +46,13 @@ function ProjectListCard({ project }: Props) {
             className="block w-full bg-white border border-gray-200 rounded-2xl shadow-sm hover:shadow-md hover:-translate-y-1 transition-all duration-300 group overflow-hidden"
         >
             {/* project-card-header */}
-            <div className="px-4 py-2 border-b border-gray-100">
-                <div className="flex justify-between items-center">
-                    <div className="flex flex-grow items-center gap-2">
-                        <h5 className="text-lg font-bold text-gray-900 group-hover:text-blue-600 transition-colors whitespace-normal break-all">
-                            {project.projectTitle}
-                        </h5>
-                    </div>
+            <div className="px-6 py-3 md:px-10 md:py-7 border-b border-gray-100">
+                <div className="flex justify-between items-start gap-6">
+                    <p className="text-lg md:text-xl font-bold text-gray-900 group-hover:text-blue-600 transition-colors truncate">
+                        {project.projectTitle}
+                    </p>
                     <span
-                        className={`${stateColor} ${textColor} px-2 py-0.5 rounded-full text-xs font-semibold flex-shrink-0`}
+                        className={`${stateColor} ${textColor} px-3 py-1 rounded-full text-xs font-semibold flex-shrink-0`}
                     >
                         {state}
                     </span>
@@ -62,71 +60,53 @@ function ProjectListCard({ project }: Props) {
             </div>
 
             {/* project-card-content */}
-            <div className="px-5 py-2">
-                <div className="grid grid-cols-5 gap-4">
+            <div className="px-6 py-3 md:px-10 md:py-7 ">
+                <div className="flex flex-col md:grid md:grid-cols-5 md:grid-rows-2 gap-2 md:gap-8">
                     {/* content-left-column */}
-                    <div className="col-span-2 space-y-2">
-                        <div className="space-y-0.5">
-                            <span className="text-[10px] font-bold text-gray-500 uppercase tracking-wider">
-                                프로젝트 기간
+                    <div className="col-span-2 row-span-1">
+                        <span className="text-xs font-bold text-gray-500 uppercase tracking-wider">
+                            프로젝트 기간
+                        </span>
+                        <div className="flex items-center text-sm text-gray-700 gap-3">
+                            <span className="font-medium">
+                                {formatYearMonth(project.start)}
                             </span>
-                            <div className="flex items-center gap-2 text-xs text-gray-700">
-                                <span className="font-medium">
-                                    {formatYearMonth(project.start)}
-                                </span>
-                                <div className="w-[15%] h-px bg-gray-300"></div>
-                                <span className="font-medium">
-                                    {formatYearMonth(project.end)}
-                                </span>
-                            </div>
+                            <div className="w-[30%] md:w-[25%] h-px bg-gray-300"></div>
+                            <span className="font-medium">
+                                {formatYearMonth(project.end)}
+                            </span>
                         </div>
+                    </div>
 
-                        <div className="space-y-1">
-                            <span className="text-xs font-bold text-gray-500 uppercase tracking-wider">
-                                현재 차수
-                            </span>
-                            <div className="flex items-center gap-2 text-xs text-gray-700">
-                                <span className="font-medium">{duration}</span>
-                            </div>
+                    <div className="col-start-1 col-span-2 row-start-2">
+                        <span className="text-xs font-bold text-gray-500 uppercase tracking-wider">
+                            현재 차수
+                        </span>
+                        <div className="flex items-center text-sm text-gray-700 gap-3">
+                            <span className="font-medium">{duration}</span>
                         </div>
                     </div>
 
                     {/* content-right-column */}
-                    <div className="col-span-3 space-y-2">
-                        <div className="space-y-0.5">
-                            <span className="text-[10px] font-bold text-gray-500 uppercase tracking-wider">
-                                프로젝트 매니저
+                    <div className="col-start-3 col-span-3 row-start-1 space-y-1">
+                        <span className="text-xs font-bold text-gray-500 uppercase tracking-wider">
+                            프로젝트 매니저
+                        </span>
+                        <div className="flex items-center gap-2">
+                            <span className="text-sm text-gray-700 font-medium">
+                                {project.managerName}
                             </span>
-                            <div className="flex items-center gap-2">
-                                <span className="text-xs text-gray-700 font-medium">
-                                    {project.managerName}
-                                </span>
-                            </div>
                         </div>
+                    </div>
 
-                        <div className="space-y-1">
-                            <div className="flex justify-between items-center">
-                                <span className="text-[10px] font-bold text-gray-500 uppercase tracking-wider">
-                                    참여 인원
-                                </span>
-                            </div>
-                            <div className="flex flex-wrap items-center gap-3">
-                                {(project.attendant ?? []).map((name, i) => (
-                                    <span
-                                        key={`${name}-${i}`}
-                                        className="text-xs text-gray-700 font-medium"
-                                    >
-                                        {name}
-                                    </span>
-                                ))}
-
-                                {(!project.attendant ||
-                                    project.attendant.length === 0) && (
-                                    <span className="text-xs text-gray-400 font-medium">
-                                        -
-                                    </span>
-                                )}
-                            </div>
+                    <div className="col-start-3 col-span-3 row-start-2 space-y-1">
+                        <span className="text-xs font-bold text-gray-500 uppercase tracking-wider">
+                            참여 인원
+                        </span>
+                        <div className="overflow-hidden text-ellipsis whitespace-nowrap text-sm text-gray-700 font-medium">
+                            {(project.attendant ?? []).join(
+                                "\u00A0\u00A0\u00A0"
+                            )}
                         </div>
                     </div>
                 </div>
