0a1f791ce92bcd873722e8d619ead1080f7f7590
shoh0011 <shoh@wise.co.kr>
2025-09-16 16:09:54 +0900

    feat : Apply mobile css to pagination

--- wise-office-client/src/pages/index.tsx
+++ wise-office-client/src/pages/index.tsx
@@ -105,9 +105,10 @@ export default function Home() {
                 <button
                     onClick={() => movePage(1)}
                     disabled={currentPage === 1}
-                    className="px-3 py-1 text-sm font-medium bg-blue-500 hover:bg-blue-600 text-white rounded-md cursor-pointer disabled:opacity-0 disabled:cursor-default"
+                    className="w-13 md:w-20 px-3 py-1 text-sm font-medium bg-blue-500 hover:bg-blue-600 text-white rounded-md cursor-pointer disabled:opacity-0 disabled:cursor-default"
                 >
-                    첫 페이지
+                    <p className="hidden md:block">첫 페이지</p>
+                    <p className="md:hidden">처음</p>
                 </button>
 
                 {/* 페이지 번호 */}
@@ -153,9 +154,10 @@ export default function Home() {
                 <button
                     onClick={() => movePage(totalPages)}
                     disabled={currentPage === totalPages}
-                    className="px-3 py-1 text-sm font-medium bg-blue-500 hover:bg-blue-600 text-white rounded-md cursor-pointer disabled:opacity-0 disabled:cursor-default"
+                    className="w-13 md:w-20 px-3 py-1 text-sm font-medium bg-blue-500 hover:bg-blue-600 text-white rounded-md cursor-pointer disabled:opacity-0 disabled:cursor-default"
                 >
-                    끝 페이지
+                    <p className="hidden md:block">끝 페이지</p>
+                    <p className="md:hidden">끝</p>
                 </button>
             </nav>
         </section>
