753677045ac49a0a0a9b84cdaacba3f471a6780b
jhryu <jhryu@gmail.com>
2026-02-25 18:56:47 +0900

    feat : Integrate yml and env

--- wise-office-client/next.config.ts
+++ wise-office-client/next.config.ts
@@ -3,8 +3,24 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     /* config options here */
     reactStrictMode: false,
+    output: "standalone",
     images: {
-        domains: ["lh3.googleusercontent.com", "localhost"],
+        remotePatterns: [
+            {
+                protocol: "http",
+                hostname: "localhost",
+                port: "8080",
+            },
+            {
+                protocol: "http", // PROD 환경에서 사용
+                hostname: "10.0.21.185", // PROD 환경 실행시 본인의 IP로 변경
+                port: "8081",
+            },
+            {
+                protocol: "https",
+                hostname: "lh3.googleusercontent.com",
+            },
+        ],
     },
 };
 
