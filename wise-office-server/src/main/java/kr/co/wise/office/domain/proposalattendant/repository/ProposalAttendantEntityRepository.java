173d09d359ff8e22a3a70df63b73382c6c386b69
jysur <m15701313@gmail.com>
2026-03-09 10:53:46 +0900

    feat : Add update proposal attendants

--- wise-office-server/src/main/java/kr/co/wise/office/domain/proposalattendant/repository/ProposalAttendantEntityRepository.java
+++ wise-office-server/src/main/java/kr/co/wise/office/domain/proposalattendant/repository/ProposalAttendantEntityRepository.java
@@ -1,11 +1,17 @@
 package kr.co.wise.office.domain.proposalattendant.repository;
 
+import kr.co.wise.office.domain.Project.entity.ProjectEntity;
 import kr.co.wise.office.domain.proposalattendant.entity.ProposalAttendantEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ProposalAttendantEntityRepository extends JpaRepository<ProposalAttendantEntity, Long> {
     List<ProposalAttendantEntity> findAllByProject_Id(Long projectId);
+
+    @Query("select a from ProposalAttendantEntity a join fetch a.companyMember where a.exitDate is null and a.project = :project")
+    List<ProposalAttendantEntity> findProposalAttendantsByProjectIdWithCompanyMember(@Param("project")ProjectEntity project);
 }
