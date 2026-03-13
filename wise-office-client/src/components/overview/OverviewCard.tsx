9c1d4f87e0ae329ba21dc30fdd3cfe03c6ec5524
shoh0011 <shoh@wise.co.kr>
2026-03-13 13:57:34 +0900

    feat : Update overview card layout

--- wise-office-client/src/components/overview/OverviewCard.tsx
+++ wise-office-client/src/components/overview/OverviewCard.tsx
@@ -14,32 +14,33 @@ export default function OverviewCard({
     isApproval,
 }: Props) {
     return (
-        <div className="flex bg-white border border-gray-300 rounded-lg px-4 py-2 items-center justify-between gap-6">
-            {/* 날짜/시간 */}
-            <div className="col-span-1 flex flex-col 2xl:flex-row gap-2">
-                <p>{minutes.minutesAt}</p>
-                <p>
-                    {" "}
-                    {minutesInfo?.startTime}~{minutesInfo?.endTime}
-                </p>
-            </div>
-
+        <div className="flex w-full bg-white border border-gray-300 rounded-lg px-4 py-2 items-center justify-between gap-6">
             {/* 문서 정보 */}
-            <div className="flex-1 grid grid-cols-2">
-                {/* 문서번호 */}
-                <p className="col-span-1 flex items-center">
-                    {isApproval && minutes?.minutesId
-                        ? `품의서 · ${minutes?.title}`
-                        : `회의록 · ${minutes?.title}`}
-                </p>
-                {/* 참석자 명단 */}
-                <p className="col-span-1 text-xs flex items-center">
-                    {minutesInfo?.instAttendants}
-                </p>
+            <div className="flex flex-1 gap-6 items-center min-w-0">
+                {/* 날짜/시간 */}
+                <div className="flex flex-col 2xl:flex-row gap-2 shrink-0">
+                    <p>{minutes.minutesAt}</p>
+                    <p>
+                        {minutesInfo?.startTime}~{minutesInfo?.endTime}
+                    </p>
+                </div>
+
+                <div className="grid grid-cols-2 flex-1 min-w-0 gap-6">
+                    <div className="col-span-1 flex justify-center">
+                        {/* 문서번호 */}
+                        {isApproval && minutes?.minutesId
+                            ? `품의서 · ${minutes?.title}`
+                            : `회의록 · ${minutes?.title}`}
+                    </div>
+                    {/* 참석자 명단 */}
+                    <div className="col-span-1 text-xs flex items-center">
+                        {minutesInfo?.instAttendants}
+                    </div>
+                </div>
             </div>
 
             {/* 버튼 */}
-            <div className="flex justify-between gap-2">
+            <div className="flex justify-between gap-2 shrink-0">
                 <PreviewButton minutesInfo={minutesInfo ?? null} />
                 <PrintButton minutesInfo={minutesInfo ?? null} />
             </div>
