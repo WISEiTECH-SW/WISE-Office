9f54c8b46a429954e2a9fef4929044b99059af6c
jysur <m15701313@gmail.com>
2026-03-27 13:39:24 +0900

    feat : Add writer on aprove list item

--- wise-office-client/src/components/project/document/list-item/ApproveItem.tsx
+++ wise-office-client/src/components/project/document/list-item/ApproveItem.tsx
@@ -40,6 +40,7 @@ export default function ApproveItem({
             </div>
 
             <div className="flex mt-2 justify-between">
+                <div className="text-xs text-gray-500">{approve.writer}</div>
                 <div className="flex items-center text-xs text-gray-500">
                     <CalendarDays className="w-3 h-3 mr-1" />
                     {formatDate(approve.submitDate)}
