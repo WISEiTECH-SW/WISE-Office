effff5c1a1f82c98e396ee2e74d1c8f1dd2855a3
shoh0011 <shoh@wise.co.kr>
2026-04-01 10:34:27 +0900

    Fix : Resolve build error

--- wise-office-client/src/middleware.ts
+++ wise-office-client/src/middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
     const token = req.cookies.get("jwt")?.value;
-    const { pathname, searchParams } = req.nextUrl;
+    const { pathname } = req.nextUrl;
 
     const protectedPath =
         pathname === "/projects" ||
