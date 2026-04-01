f2adc5e6d79adeee3b96bcef40553aa1b7048208
jysur <m15701313@gmail.com>
2026-04-01 11:07:57 +0900

    fix : Apply feedback

--- wise-office-client/src/components/project/document/list-item/MinuteItem.tsx
+++ wise-office-client/src/components/project/document/list-item/MinuteItem.tsx
@@ -28,7 +28,7 @@ export default function MinuteItem({
                 <h3 className="font-medium text-sm text-gray-800 truncate mr-1">
                     {minute.purpose}
                 </h3>
-                <h3 className="font-small text-xs text-gray-500 truncate pt-1">
+                <h3 className="font-normal text-xs text-gray-500 truncate pt-1">
                     · {minute.title}
                 </h3>
             </div>
