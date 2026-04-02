7d6586e6cf341ec2d224117b4fd5fc2d9b83cae5
jysur <m15701313@gmail.com>
2026-04-02 10:59:17 +0900

    fix : Apply feedback

--- wise-office-client/src/components/leave-tracker/OutputSection/SummaryCard.tsx
+++ wise-office-client/src/components/leave-tracker/OutputSection/SummaryCard.tsx
@@ -7,7 +7,7 @@ type SummaryCardProps = {
 export default function SummaryCard({ title, count, bg }: SummaryCardProps) {
     return (
         <div
-            className={`w-full h-18 md:h-18 border rounded-lg border-gray-300 py-2 flex flex-col items-center justify-around ${bg}`}
+            className={`w-full h-[72px] md:h-[72px] border rounded-lg border-gray-300 py-2 flex flex-col items-center justify-around ${bg}`}
         >
             <p className="text-xs lg:text-sm font-medium">{title}</p>
             <p className="text-base lg:text-xl font-bold">{count}</p>
